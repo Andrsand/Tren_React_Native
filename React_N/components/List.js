1414b7a7dba0bb87ad8ad0af958c0147bbd72441
andrsand <angorko@list.ru>
2022-06-25 19:10:05 +0300

    next change

--- React_N/components/List.js
+++ React_N/components/List.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import {StyleSheet, View} from 'react-native'
-import Header from './components/Header'
 
 export default function App() {
     return (
         <View>
-          <Header />
+
         </View>
     );
 }
