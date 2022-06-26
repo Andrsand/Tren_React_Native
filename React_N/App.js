70176808d8842747bd8af9adf19a0c7fc9157cfc
andrsand <angorko@list.ru>
2022-06-26 16:28:33 +0300

    Added to-do list array to Add.js

--- React_N/App.js
+++ React_N/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import {StyleSheet, View} from 'react-native'
+import React, { useState } from 'react'
+import {StyleSheet, View, Text, FlatList} from 'react-native'
 import Header from './components/Header'
 
+
 export default function App() {
+  const [listOfItems, setListOfItems] = useState([
+    {text: 'Купить молоко', index: 1},
+    {text: 'Помыть машину', index: 2},
+    {text: 'Купить картошку', index: 3},
+    {text: 'Стать миллионером', index: 4}
+  ])
+
     return (
         <View>
           <Header />
+          <View>
+            <FlatList data={listOfItems} renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )} />
+          </View>
         </View>
     );
 }
