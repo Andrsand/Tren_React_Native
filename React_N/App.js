81ad81ee31776f9d843f82ded754cb9f428febd1
andrsand <angorko@list.ru>
2022-06-27 18:06:00 +0300

    feat: Changes in Add.js and List.js
    * list.js renamed in ListItem.js
    * Added to Add.js <ListItem /> component
    *Are registered
     styles for <ListItem />

--- React_N/App.js
+++ React_N/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {StyleSheet, View, Text, FlatList} from 'react-native'
 import Header from './components/Header'
+import ListItem from './components/ListItem'
 
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
           <Header />
           <View>
             <FlatList data={listOfItems} renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <ListItem el={item}/>
             )} />
           </View>
         </View>
