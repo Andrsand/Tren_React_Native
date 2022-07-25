198586d4243f732e4abe6abd4dcc1b6ccdab2175
andrsand <angorko@list.ru>
2022-07-25 14:43:33 +0300

    feature:  Card Component
    * Created ui folder with AppCard.js file
    * Styles added to AppCard.js and TodoScreen.js files

--- Todo/src/screens/TodoScreen.js
+++ Todo/src/screens/TodoScreen.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
+import {THEME} from '../theme'
+import { AppCard} from '../components/ui/AppCard'
 
 // экспорт константы TodoScreen принимающей свойства и возвращающей JSX
 export const TodoScreen = ({ goBack, todo }) => { 
     return (
       <View>
-        <Text>{todo.title}</Text>
+        <AppCard style={styles.card}>
+          <Text style={styles.title}>{todo.title}</Text>
+          <Button title='Ред.'/>
+        </AppCard>
+        
         <View style={styles.buttons}>
           <View style={styles.button}>
-            <Button title='Назад' onPress={goBack} color='#757575' />
+            <Button title='Назад' onPress={goBack} color={THEME.GREY_COLOR} />
           </View>
           <View style={styles.button}>
             <Button
               title='Удалить'
-              color='#e53935'
+              color={THEME.DANGER_COLOR}
               onPress={() => console.log('To Remouve')}
             />
           </View>
         </View>
         
       </View>
-    )
+    ) 
 }
 
 const styles = StyleSheet.create({
@@ -28,7 +34,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
   button: {
     width: '40%'
+  },
+  title: {
+    fontSize: 20
   }
+
 })
