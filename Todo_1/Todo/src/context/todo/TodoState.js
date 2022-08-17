76a8278b267c0ac34769ccfce90f1883c43aea4f
andrsand <angorko@list.ru>
2022-08-17 14:45:14 +0300

    refactor : Refactoring the application
    * File created: src/MainLayout.js
    * Changed files: App.js, todoContext.js, todoReducer.js, TodoState,js

--- Todo_1/Todo/src/context/todo/TodoState.js
+++ Todo_1/Todo/src/context/todo/TodoState.js
@@ -2,22 +2,19 @@ import React, { useReducer } from 'react'
 import { TodoContext } from './todoContext'
 import { todoReducer } from './todoReducer'
 
-export const TodoContext = ({ children }) => {
-    const initialState = {  
-        todos: [ { id: '1', title: 'Доучить React Native' }]
-    }
-    const [state, dispatch] = useReducer(todoReducer, initialState)
-    
-    return (
-    <TodoContext.Provider 
+export const TodoState = ({ children }) => {
+  const initialState = {
+    todos: [{ id: '1', title: 'Выучить React Native' }]
+  }
+  const [state, dispatch] = useReducer(todoReducer, initialState)
+
+  return (
+    <TodoContext.Provider
       value={{
         todos: state.todos
       }}
     >
-        {children}
+      {children}
     </TodoContext.Provider>
   )
 }
-
-
- 
