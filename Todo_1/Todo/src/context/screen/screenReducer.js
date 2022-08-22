a5d473583da74b52c71a3429180dbd3a147ab8bc
Andrsand <angorko@list.ru>
2022-08-22 23:39:17 +0300

    refactoring:  Screen change
    * Changed screenReducer.js file

--- Todo_1/Todo/src/context/screen/screenReducer.js
+++ Todo_1/Todo/src/context/screen/screenReducer.js
@@ -1,3 +1,9 @@
+import { CHANGE_SCREEN } from '../types'
+
+const hadlers = {
+    [CHANGE_SCREEN]: (state, payload) => payload
+}
+
 export const screenReducer = (state, action) => {
     return state
 }
