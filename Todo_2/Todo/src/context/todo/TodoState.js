be76cd92ecb165e43107740e183de0d5bb30fc78
andrsand <angorko@list.ru>
2022-09-16 21:39:38 +0300

    Helper functions
    *  Adding helper functions to TodoState.js and todoReduser.js

--- Todo_2/Todo/src/context/todo/TodoState.js
+++ Todo_2/Todo/src/context/todo/TodoState.js
@@ -1,10 +1,11 @@
 import React, { useReducer, useContext } from 'react'
 import { Alert } from 'react-native'
 import { ScreenContext } from '../screen/screenContext'
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import { ADD_TODO, CLEAR_ERROR, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO } from '../types'
 import { TodoContext } from './todoContext'
 import { todoReducer } from './todoReducer'
 
+// стейт для работы с сервером и логикой
 export const TodoState = ({ children }) => {
   const initialState = {
     todos: [],
@@ -39,7 +40,19 @@ export const TodoState = ({ children }) => {
     )
 
   }
+
+
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
+
+  // эти функции ￬ - вспомогательные и приватные для стейта ￪ TodoState
+
+  const showLoader = () => dispatch({ type: SHOW_LOADER }) // функция показывает Loader. Она вызывается из todoReducer.js и флаг loading со значением true
+
+  const hideLoader = () => dispatch({ type: HIDE_LOADER }) // этот dispatch делает тоже самое, только убирает Loader и меняет только один флаг в стейте на значение false
+
+  const showError = error => dispatch({ type: SHOW_ERROR, error }) // в параметр error принимаем текст ошибки и выводим его 
+
+  const clearError = () => dispatch({ type: CLEAR_ERROR }) // функция не принимает параметр, просто диспатчит CLEAR_ERROR
 
   return (
     <TodoContext.Provider
--- Todo_2/Todo/src/context/todo/todoReducer.js
+++ Todo_2/Todo/src/context/todo/todoReducer.js
@@ -1,7 +1,17 @@
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types"
+import {
+  ADD_TODO,
+  CLEAR_ERROR,
+  FETCH_TODOS,
+  HIDE_LOADER,
+  REMOVE_TODO,
+  SHOW_ERROR,
+  SHOW_LOADER,
+  UPDATE_TODO
+} from "../types"
 
 const handlers = {
-  [ADD_TODO]: (state, {title}) => ({...state,
+  [ADD_TODO]: (state, { title }) => ({
+    ...state,
     todos: [
       ...state.todos,
       {
@@ -10,10 +20,11 @@ const handlers = {
       }
     ]
   }),
-  [REMOVE_TODO]: (state, {id}) => ({...state,
+  [REMOVE_TODO]: (state, { id }) => ({
+    ...state,
     todos: state.todos.filter(todo => todo.id !== id)
   }),
-  [UPDATE_TODO]: (state, {title, id}) => ({
+  [UPDATE_TODO]: (state, { title, id }) => ({
     ...state,
     todos: state.todos.map(todo => {
       if (todo.id === id) {
@@ -22,6 +33,11 @@ const handlers = {
       return todo
     })
   }),
+  [SHOW_LOADER]: state => ({ ...state, loading: true }), // когда вызывается экшн SHOW_LOADER в стейте меняется флаг loading на true 
+  [HIDE_LOADER]: state => ({ ...state, loading: false }), // при вызове экшн HIDE_LOADER изменяем флаг на false
+  [CLEAR_ERROR]: state => ({ ...state, error: null }),      // при вызове экшн CLEAR_ERROR - чистим ошибку
+  [SHOW_ERROR]: (state, { error }) => ({ ...state, error }),  // экшн показываеющий ошибку
+  [FETCH_TODOS]: (state, { todos }) => ({ ...state, todos }), // экшн вызвается, когда приходит список готовых todo с сeрвера. Заменяем массив todos на тот массив, который нам прилетает. 
   DEFAULT: state => state
 }
 
