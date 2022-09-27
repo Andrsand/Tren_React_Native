12f1b21f7c2a2e51baa063d135c5e91254b532c9
andrsand <angorko@list.ru>
2022-09-27 23:23:30 +0300

    feature: Load indicator
    * Created ui/AppLoader.js file
    * Export constant Apploader in Apploader.js

---
+++ Todo_2/Todo/src/components/ui/AppLoader.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { StyleSheet, View, ActivityIndicator } from "react-native"
+
+
+export const AppLoader = () => (
+    <View>
+        <ActivityIndicator /> {/* компонент загрузки */}
+    </View>
+)
