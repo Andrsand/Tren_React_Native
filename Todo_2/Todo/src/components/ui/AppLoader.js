ceff0a25432d293f858a7137b74fc2137ec88cdb
andrsand <angorko@list.ru>
2022-09-28 21:37:37 +0300

    Adding Styles
    * Adding styles to AppLoader.js

--- Todo_2/Todo/src/components/ui/AppLoader.js
+++ Todo_2/Todo/src/components/ui/AppLoader.js
@@ -3,7 +3,15 @@ import { StyleSheet, View, ActivityIndicator } from "react-native"
 
 
 export const AppLoader = () => (
-    <View>
+    <View style={styles.center}>
         <ActivityIndicator /> {/* компонент загрузки */}
     </View>
 )
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
